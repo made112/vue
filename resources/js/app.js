3cb22e3b6c84857bfe0e4900b66d799967686963
ENG mohammed <mmsh7398@gmail.com>
2020-09-23 03:11:12 +0300

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import App from './App.vue';
+// import BootstrapVue from 'bootstrap-vue';
+// Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap' ;
+
+ new Vue({
+    el: '#app',
+     render:h=>h(App)
+});
+
+
